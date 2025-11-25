64dd73c008e903ebef041fda8611f7ca80caef77
Danilo Tadeu Orige Oliveira <daniloorige@gmail.com>
2025-11-25 18:37:55 -0300

    Filtro de referência ajustado + correções

--- Aerogerador.m
+++ Aerogerador.m
@@ -18,8 +18,8 @@ beta = 0; % [rad] Ã‚ngulo de pitch das pÃ¡s
 
 % Valores hipotÃ©ticos
 
-Ea = 500;  % [V]
-U = 10; % [m/s] 
+Ea = 1637;  % [V]
+U = 10.31; % [m/s] 
 %%
 %---------------QUESTÃO 0.1--------------------------------------
 
@@ -29,10 +29,9 @@ beta0 = 0; %[graus]
 beta1 = 15; %[graus]
 beta2 = 30; %[graus]
 
-% PESQUISAR BETAS QUE SÃO USADOS NORMALMENTE
 
 lambda = linspace(0, 15, 100);
-betas = [0, beta1*pi/180, beta2*pi/180];
+betas = [beta0*pi/180, beta1*pi/180, beta2*pi/180];
 Cp = zeros(length(lambda), length(betas));
 
 
@@ -48,12 +47,12 @@ Cpmax15 = Cpmax(:, 2);
 Cpmax30 = Cpmax(:, 3);
 
 
-figure;
+figure('Name', 'Curvas Cp x betas');
 plot(lambda, Cp, 'LineWidth', 1.5);
 grid on;
 
-xlabel('\lambda (razão de velocidade de ponta)');
-ylabel('C_p (coeficiente de potência)');
+xlabel('\lambda (razão de velocidade de ponta) ');
+ylabel('Cp [%](coeficiente de potência)');
 title('Curvas de C_p(\lambda, \beta)');
 
 
@@ -61,28 +60,20 @@ legend(arrayfun(@(b) sprintf('\\beta = %.1f', b), betas*180/pi, 'UniformOutput',
        'Location', 'best');
 
 
+
 %%
-%lambda = R*w/U;
-%Cp = (0.44 - 0.167*beta)*sin((pi*lambda)/(15 - 0.3*beta)) - 0.16*lambda*beta;
+
 funCp = @(lambda, beta)((0.44 - 0.167*beta)*sin((pi*lambda)/(15 - 0.3*beta)) - 0.16*lambda*beta);
 
 Pw = 0.5*rho*A*U^3;
 
-% Pm = tau*w = Cp*Pw
-% tau = Cp*Pw/w
-% tau = (Cp * Pw)/w;
-
 funTau = @(w) ((funCp(R*w/U, beta) * Pw)/w);
-
-% ./w  ao inves de /w faria multiplicaÃ§Ã£o elemento a elemento? perguntar
-% pro lelis
-% .* Ã© o equivalente para multiplicaÃ§Ã£o
 
 %%
 %-----------------------QUESTÃO 2----------------------------------
-% Feito substituindo uma edo na outra e removendo dependencia de ia em w
+
 w0 = 1;   % CI 
-% se CI = 0 da erro porque tau/0
+
 
 w = fzero(@(w) funTeste(w, funTau, b, kv, Ra, Ea), w0);
 
@@ -140,7 +131,7 @@ for k = 1:length(Uvec_menor)
 end
 
 % Plot 1
-figure;
+figure('Name', 'Comportamento do Aerogerador na Região Subnominal (U < Unom)');
 
 subplot(2,1,1);
 plot(Uvec_menor, Ea_vec, 'g', 'LineWidth', 1.5);
@@ -178,11 +169,11 @@ end
 
 % Plot 2 
 
-figure;
+figure('Name', 'Comportamento do Aerogerador na Região Sobrenominal (U > Unom)');
 subplot(2,1,1);
 plot(Uvec_maior, Beta_vec*180/pi, 'LineWidth', 1.5)
 xlabel('Velocidade do vento U [m/s]');
-ylabel('\beta  [graus]');
+ylabel('\beta  [°]');
 grid on;
 
 subplot(2,1,2);
@@ -191,7 +182,7 @@ xlabel('Velocidade do vento U [m/s]');
 ylabel('Velocidade angular \omega [rad/s]');
 grid on;
 
-sgtitle('Comportamento do Aerogerador na Região Subnominal (U > Unom)', ...
+sgtitle('Comportamento do Aerogerador na Região Sobrenominal (U > Unom)', ...
     'FontWeight','bold', 'FontSize',12);
 
 %%
@@ -199,12 +190,12 @@ sgtitle('Comportamento do Aerogerador na Região Subnominal (U > Unom)', ...
 %-----------------------QUESTÃO 9----------------------------------
 % DINAMICA
 
-[tvec, xvec] = ode45(@(t,x) funDin_aerogerador(J, funTau, b, La, Ra, Ea, kv, x), [0 10], [0 1]);
+[tvec, xvec] = ode45(@(t,x) funDin_aerogerador(J, U_nom, Cpmax0, rho, A, b, La, Ra, Ea, kv, x), [0 10], [0.0001 0.0001]);
 iavec = xvec(:,1);
 wvec = xvec(:,2);
 
 % Plot dinâmica
-
+figure('Name', 'Dinâmica das Variáveis de Estado');
 subplot(2,1,1)
 plot(tvec, iavec, 'r', 'LineWidth', 1.5)
 xlabel('Tempo [s]')
@@ -214,7 +205,7 @@ grid on
 subplot(2,1,2)
 plot(tvec, wvec, 'b', 'LineWidth', 1.5)
 xlabel('Tempo [s]')
-ylabel('Velocidade de rotação [rad/s]')
+ylabel('Velocidade angular [rad/s]')
 grid on
 
 sgtitle('Dinâmica das Variáveis de Estado', 'FontWeight', 'bold', ...
@@ -232,7 +223,8 @@ q_10a= out.perturbacao10a;
 
 w_10b = out.questao10b;
 w_ref10b = out.w_ref10b;
-u_10b = out.controle10b;
+Ea_10b = out.Ea10b;
+beta_10b = out.beta10b;
 q_10b = out.perturbacao10b;
 
 t_simulink = out.tout;
@@ -241,20 +233,20 @@ t_simulink = out.tout;
 % Plot questão 10a
 
 
-figure;
+figure('Name' ,'Ensaio resposta ao degrau com U = Unominal e referência variável');
 subplot(3,1,1)
 plot(t_simulink, w_10a, 'g', 'LineWidth', 1.5)
 xlabel('Tempo [s]')
-ylabel('Velocidade angular das pás [rad/s]')
+ylabel('Velocidade angular [rad/s]')
 grid on
 hold on
 plot(t_simulink, w_ref10a, 'Color' ,'#FF0000', 'LineWidth', 2.0, 'LineStyle','--');
-legend('Resposta do sistema', 'Refência')
+legend('Resposta do sistema', 'Refência', 'location', 'southeast')
 
 subplot(3,1,2)
 plot(t_simulink, u_10a, 'b', 'LineWidth', 1.5)
 xlabel('Tempo [s]')
-ylabel('Entrada de controle')
+ylabel('Controle')
 grid on
 
 subplot(3,1,3)
@@ -262,50 +254,46 @@ plot(t_simulink, q_10a, 'm', 'LineWidth', 1.5)
 xlabel('Tempo [s]')
 ylabel('Velocidade do vento [m/s]')
 grid on
-sgtitle('Ensaio resposta ao degrau com U = Unominal e referência variável', 'FontWeight', 'bold', ...
-    'FontSize', 12)
+% sgtitle('Ensaio resposta ao degrau com U = Unominal e referência variável', 'FontWeight', 'bold', ...
+%     'FontSize', 12)
 
 
-figure;
-subplot(3,1,1)
+figure('Name', 'Ensaio de resposta ao degrau com referência fixa e U variável');
+subplot(2,2,1)
 plot(t_simulink, w_10b, 'Color', '#F1C338', 'LineWidth', 1.5)
 xlabel('Tempo [s]')
-ylabel('Velocidade angular das pás [rad/s]')
+ylabel('Velocidade angular[rad/s]')
 grid on
 hold on
 plot(t_simulink, w_ref10b,'Color' ,'#FF00FF', 'LineWidth', 2.0, 'LineStyle','--');
-legend('Resposta do sistema', 'Refência')
+legend('Resposta do sistema', 'Refência', 'location', 'southeast')
 
-subplot(3,1,2)
-plot(t_simulink, u_10b, 'b', 'LineWidth', 1.5)
+subplot(2,2,2)
+plot(t_simulink, Ea_10b, 'b', 'LineWidth', 1.5)
 xlabel('Tempo [s]')
-ylabel('Entrada de controle')
+ylabel('Ea - Tensão na armadura [V]')
 grid on
 
-subplot(3,1,3)
+subplot(2,2,3)
 plot(t_simulink, q_10b, 'm', 'LineWidth', 1.5)
 xlabel('Tempo [s]')
 ylabel('Velocidade do vento [m/s]')
 grid on
 
+beta_10b(341:end) = 0;
 
 
-sgtitle('Ensaio de resposta ao degrau com referência', 'FontWeight', 'bold', ...
-    'FontSize', 12)
+subplot(2,2,4)
+plot(t_simulink, beta_10b*(180/pi), 'g', 'LineWidth', 1.5)
+xlabel('Tempo [s]')
+ylabel('\beta - Ângulo de pitch [°]')
+grid on
+
+% 
+% sgtitle('Ensaio de resposta ao degrau com referência fixa e U variável', 'FontWeight', 'bold', ...
+%     'FontSize', 12)
 %%
 %-----------------------QUESTÃO 11----------------------------------
-% 
-% Uvec = (3:20)';
-% vectempo = (0:4.5:76.5)';
-% 
-% dadosts = timeseries(vectempo, Uvec);
-% dados_ts.UserData.Interpolation = 'zoh';
-% 
-% stairs(vectempo, Uvec, 'r', 'LineWidth', 1.5)
-% 
-% xlabel('Tempo [s]')
-% ylabel('Vento [m/s]')
-% grid on
 
 vecU11 = linspace(3, 20, 18);
 vecU11 = [3, vecU11];
@@ -323,24 +311,41 @@ q11 = out.perturbacao11;
 w_ref11 = out.w_ref11;
 Pentregue11 = out.potencia11;
 ia11 = out.ia11;
+pert_11 = out.perturbacao11;
 
 
-figure;
-subplot(2,1,1)
-plot(tout, w11, 'Color', '#FF0000', 'LineWidth', 1.5)
+figure('Name', 'Comportamento do sistema para a faixa de vento [3,20]m/s');
+subplot(3,1,1)
+plot(tout, w11, 'Color', '#FF0000', 'LineWidth', 1.5, 'DisplayName', 'w [rad/s]')
 xlabel('Tempo [s]')
-ylabel('Velocidade angular nas pás [rad/s]')
+ylabel('Velocidade angular [rad/s]')
 grid on
 hold on
-plot(tout, w_ref11, 'g', 'LineWidth', 1.5, 'LineStyle', ':')
+plot(tout, w_ref11, 'g', 'LineWidth', 1.5, 'LineStyle', ':', 'DisplayName','wref [rad/s]')
+legend show;
+legend ('Location', 'southeast')
 
-subplot(2,1,2)
-plot(tout, Ea11, 'Color', '#FF0000', 'LineWidth', 1.5)
+subplot(3,1,2)
+plot(tout, Ea11, 'Color', '#FF0000', 'LineWidth', 1.5, 'DisplayName', 'Ea [V]')
 xlabel('Tempo [s]')
-ylabel('Entradas de controle')
+ylabel('Tensão na armadura [V]')
 grid on 
 hold on
-plot(tout, beta11*(180/pi)*100, 'Color', '#FF5C00', 'LineWidth', 1.5)
+legend show;
+legend ('Location', 'southeast')
+
+subplot(3,1,3)
+plot(tout, beta11*(180/pi), 'Color', '#FF5C00', 'LineWidth', 1.5, 'DisplayName', '\beta [°]')
+xlabel('Tempo [s]')
+ylabel('Ângulo de pitch [°]')
+grid on
+hold on
+legend show;
+legend ('Location', 'southeast')
+% 
+% sgtitle('Comportamento do sistema para a faixa de vento [3,20]m/s', 'FontWeight', 'bold', ...
+%     'FontSize', 12)
+
 
 
 vecPpe11 = ones(size(tout));
@@ -353,12 +358,12 @@ Ppe11 = ((ia11 .* ia11) * Ra); % Perdas elétricas
 
 Ppm11 = (b* (w11 .* w11)); % Perdas mecânicas
  
-Pg11 = (kv*ia11.*w11); % Potência gerada
+Pg11 = kv*(ia11.*w11); % Potência gerada
 
 Pe11 = Pg11 - Ppe11 - Ppm11; % Potência entregue
  
 
-figure;
+figure('Name', 'Parcelas de Potência');
 grid on 
 hold on
 xlabel('Tempo [s]')
@@ -368,16 +373,47 @@ plot(tout, Ppm11, 'Color', '#FF5C00', 'LineWidth', 2.0, 'DisplayName', 'Perdas M
 plot(tout, Pg11, 'Color', '#50C878', 'LineWidth', 2.0, 'DisplayName', 'Potência Gerada')
 plot(tout, Pentregue11, 'Color', '#D20A2E', 'LineWidth', 2.0, 'DisplayName', 'Potência Entregue')
 legend show;
+legend('Location', 'northwest');
 
+% sgtitle('Parcelas de Potência', 'FontWeight', 'bold', ...
+%     'FontSize', 12)
 
- 
-
+figure('Name', 'Recriação da curva de potência da WEG')
+grid on
+hold on
+xlabel('Velocidade do vento [m/s]')  
+ylabel('Potência entregue à carga/rede [W]')
+plot(pert_11, Pentregue11, 'Color', '#D20A2E', 'LineWidth', 2.0, 'DisplayName', 'Potência Entregue')
+legend show;
+legend('Location', 'northwest');
 
 %%
+%----------------------- BATCH EXPORT ----------------------------------
 
+pastas   = 'Plots';
+extensao = '.pdf';   
 
+graficos = findall(groot, 'Type', 'figure');
 
+if isempty(graficos)
+    disp('Nenhuma figura encontrada para salvar.');
+else
+    for i = 1:length(graficos)
+        graf_atual = graficos(i);
+        nome_base = graf_atual.Name;
 
+        % Limpa o nome para ser um nome de arquivo válido
+        nome_base = strtrim(nome_base);
+        nome_base = regexprep(nome_base, '[^\w\-]', '_');
+
+        % Monta o nome final do arquivo
+        nome_arquivo = [nome_base extensao];
+        filePath     = fullfile(pastas, nome_arquivo);
+
+        % Salva em vetor
+        exportgraphics(graf_atual, filePath, 'ContentType', 'vector');
+    end
+end
 
 
 
